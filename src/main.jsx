b956de4405f65a8ced8986984a4c0710cd8e765d
DennerDuarte <rm551938@fiap.com.br>
2023-09-06 19:35:27 -0300

    Rotas importadas para main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import Home from './routes/Home/index.jsx'
+import Error from './routes/Error/index.jsx'
+import EditarProdutos from './routes/EditarProdutos/index.jsx'
+import Produtos from './routes/Produtos/index.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
